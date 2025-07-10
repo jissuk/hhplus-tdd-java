62ed766030636796a81145296b60d1c4ff42e497
DESKTOP-A9G14KA\Admin <dkljjh10@gmail.com>
2025-07-11 05:47:18 +0900

    test: 포인트 서비스 동시성 테스트 작성

--- src/test/java/io/hhplus/tdd/point/PointServiceIntegrationTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceIntegrationTest.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 import static org.assertj.core.api.Assertions.*;
 
 
@@ -12,6 +16,9 @@ import static org.assertj.core.api.Assertions.*;
  * */
 @SpringBootTest
 public class PointServiceIntegrationTest {
+
+    @Autowired
+    private PointService pointService;
 
     @Autowired
     private UserPointTable userPointTable;
@@ -38,9 +45,9 @@ public class PointServiceIntegrationTest {
     void 포인트_사용로직_테스트(){
 
         // given
-        long defalutAmount = 10000L;
         long id = 1L;
         long amount = 3000L;
+        long defalutAmount = 10000L;
         userPointTable.insertOrUpdate(id, defalutAmount);
 
         // when
@@ -53,4 +60,82 @@ public class PointServiceIntegrationTest {
         assertThat(resultPoint.getId()).isEqualTo(id);
         assertThat(resultPoint.getPoint()).isEqualTo(userPoint.getPoint());
     }
+
+    @Test
+    void 동시에_100번_요청해도_포인트_충전되는지_테스트() throws InterruptedException {
+        // given
+        long id = 1L;
+        long amount = 100L;
+        int threadCount = 100;
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+
+        // when
+        for (int i = 0; i < threadCount; i++) {
+            executor.submit(() -> pointService.chargePoint(id, amount));
+        }
+
+        executor.shutdown();
+        boolean finished = executor.awaitTermination(1, TimeUnit.MINUTES);
+        UserPoint resultPoint  = pointService.selectById(id);
+
+        // then
+        assertThat(finished).isTrue();
+        assertThat(resultPoint.getPoint()).isEqualTo(10000L);
+    }
+
+    @Test
+    void 동시에_100번_요청해도_포인트_사용되는지_테스트() throws InterruptedException {
+        // given
+        long id = 1L;
+        long amount = 100L;
+        long defalutAmount = 20000L;
+        userPointTable.insertOrUpdate(id, defalutAmount);
+
+        int threadCount = 100;
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+
+        // when
+        for (int i = 0; i < threadCount; i++) {
+            executor.submit(() -> pointService.usePoint(id, amount));
+        }
+
+        executor.shutdown();
+        boolean finished = executor.awaitTermination(1, TimeUnit.MINUTES);
+        UserPoint resultPoint  = pointService.selectById(id);
+
+        // then
+        assertThat(finished).isTrue();
+        assertThat(resultPoint.getPoint()).isEqualTo(10000L);
+    }
+
+    @Test
+    void 동시에_100번_요청해도_포인트_충전및사용되는지_테스트() throws InterruptedException {
+        // given
+        long id = 1L;
+        long chargeAmount = 100L;          // 100 포인트 50번 충전
+        long useAmount = 200L;             // 200 포인트 50번 사용
+        long defalutAmount = 30000L;       // 시작 금액
+        userPointTable.insertOrUpdate(id, defalutAmount);
+
+        int threadCount = 50;
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+
+        // when
+        for (int i = 0; i < threadCount; i++) {
+            executor.submit(() -> pointService.chargePoint(id, chargeAmount));
+        }
+
+        for (int i = 0; i < threadCount; i++) {
+            executor.submit(() -> pointService.usePoint(id, useAmount));
+        }
+
+        executor.shutdown();
+        boolean finished = executor.awaitTermination(1, TimeUnit.MINUTES);
+        UserPoint resultPoint  = pointService.selectById(id);
+
+        // then
+        assertThat(finished).isTrue();
+        assertThat(resultPoint.getPoint()).isEqualTo(25000L);
+    }
+
 }
